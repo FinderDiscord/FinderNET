73bf6f04695caecbf293f004839736d1bbe1f8b3
JakeyGilly <jakegillman3@gmail.com>
2022-02-13 21:01:53 +0000

    git ignore and removing files

--- modules/Help.cs
+++
@@ -1,9 +0,0 @@
-using Discord.Commands;
-using Discord.WebSocket;
-
-public class HelpModule : ModuleBase<SocketCommandContext> {
-    [Command("bleep")]
-    public async Task BleepAsync() {
-        await ReplyAsync("Bleep bloop");
-    }
-}
