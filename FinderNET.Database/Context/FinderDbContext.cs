f90281f7093b640d08469ddfb052d5f98625946c
Jake Gillman <57807696+JakeyGilly@users.noreply.github.com>
2022-05-24 20:32:07 +0000

    fixed a particular bug

--- FinderNET.Database/Context/FinderDbContext.cs
+++ FinderNET.Database/Context/FinderDbContext.cs
@@ -2,7 +2,7 @@ using FinderNET.Database.Models;
 using Microsoft.EntityFrameworkCore;
 
 namespace FinderNET.Database {
-    class FinderDbContext : DbContext {
+    public class FinderDbContext : DbContext {
         public FinderDbContext(DbContextOptions options) : base(options) { }
         public DbSet<Guild> Guilds { get; set; }
     }
