dbdc43a46cde4cdc90d1631508774d91bcde7609
Jake Gillman <57807696+JakeyGilly@users.noreply.github.com>
2022-05-27 14:42:45 +0000

    chnaged it a bit

--- FinderNET/Modules/Blackjack.cs
+++ FinderNET/Modules/Blackjack.cs
@@ -2,54 +2,52 @@ using Discord;
 using Discord.Interactions;
 using Discord.WebSocket;
 using Discord.Rest;
-using System.Linq;
 
 namespace FinderNET {
     public class BlackjackModule : InteractionModuleBase<SocketInteractionContext> {
         private static List<Blackjack> games = new List<Blackjack>();
+        private static List<String> validEmotes = new List<string>() { "✅", "❌" };
         [SlashCommand("blackjack", "Play Blackjack")]
-        public async Task Blackjack() {
+        public async Task BlackjackCommand() {
             await RespondAsync("Blackjack is not implemented yet.");
             SocketInteractionContext ctx = new SocketInteractionContext(Context.Client, Context.Interaction);
             RestUserMessage message = await ctx.Channel.SendMessageAsync($"{ctx.User.Mention} has started a game of Blackjack! React :white_check_mark: to join!");
             await message.AddReactionAsync(new Emoji("✅"));
-            games.Add(new Blackjack(ctx, message.Id, message.Channel.Id, ctx.User));
+            games.Add(new Blackjack(ctx, ctx.User));
         }
 
         public static async Task OnReactionAdded(Cacheable<IUserMessage, ulong> arg1, Cacheable<IMessageChannel, ulong> arg2, SocketReaction reaction) {
-            bool ValidMessage = false;
             foreach (Blackjack game in games){
-                if (game.JoinChannelID == reaction.Channel.Id && game.JoinMessageID == reaction.MessageId) {
-                    ValidMessage = true;
-                    break;
-                }
-            } if(!ValidMessage) {return;}
-            if (reaction.Emote.Name == "✅") {
-                await LoggingService.LogAsync(new LogMessage(LogSeverity.Info, "Blackjack", $"{reaction.User.Value.Username} has joined the game!"));
-                foreach (Blackjack game in games) {
-                    if (game.JoinChannelID == reaction.Channel.Id && game.JoinMessageID == reaction.MessageId) {
-                        game.AddPlayer(reaction.User.Value);
+                if (reaction.User.Value.IsBot) return;
+                bool valid = false;
+                foreach (string symbol in validEmotes) {
+                    if (reaction.Emote.Name == symbol) {
+                        valid = true;
                         break;
                     }
                 }
+                if (!valid) return;
+                if (game.playChannel == null || game.lobbyMessage == null) continue;
+                if (game.playChannel.Id != reaction.Channel.Id) continue;
+                if (game.lobby && reaction.MessageId == game.lobbyMessage.Id) {
+                    if (reaction.Emote.Name == "✅") {
+                        await LoggingService.LogAsync(new LogMessage(LogSeverity.Info, "Blackjack", $"{reaction.User.Value.Username} has joined the game!"));
+                        game.AddPlayer(reaction.User.Value);
+                        return;
+                    }
+                }
             }
-
-        } 
-
+        }
     }
 
     public class Blackjack {
-        public ulong JoinChannelID {get; set;}
-        public ulong JoinMessageID {get; set;}
-        ulong PlayChannelID;
-        ulong PlayMessageID;
         private SocketInteractionContext ctx;
+        public IUserMessage? lobbyMessage;
+        public IUserMessage? playMessage;
+        public ITextChannel? playChannel;
         List<IUser> players;
-        RestTextChannel? channel;
-        bool started = false;
-        public Blackjack(SocketInteractionContext _ctx, ulong _JoinMessageID, ulong _JoinChannelID, IUser creator){
-            this.JoinMessageID = _JoinMessageID;
-            this.JoinChannelID = _JoinChannelID;
+        public bool lobby = true;
+        public Blackjack(SocketInteractionContext _ctx, IUser creator){
             this.players = new List<IUser>();
             this.players.Add(creator);
             this.ctx = _ctx;
@@ -57,36 +55,28 @@ namespace FinderNET {
         }
 
         private async void newChannel(IUser creator){
-            channel = await ctx.Guild.CreateTextChannelAsync("blackjack", (x) => {
+            playChannel = await ctx.Guild.CreateTextChannelAsync("blackjack", (x) => {
                 x.Topic = "Blackjack";
                 x.PermissionOverwrites = new List<Overwrite> {
                     new Overwrite(ctx.Guild.EveryoneRole.Id, PermissionTarget.Role, new OverwritePermissions(viewChannel: PermValue.Deny, sendMessages: PermValue.Deny)),
                     new Overwrite(creator.Id, PermissionTarget.User, new OverwritePermissions(viewChannel: PermValue.Allow))
                 };
             });
-            await channel.SendMessageAsync($"You are waiting in a lobby for players to join!");
-            RestUserMessage message = await channel.SendMessageAsync($"{creator.Mention}, react :white_check_mark: to start, or :x: to cancel.");
-            await message.AddReactionAsync(new Emoji("✅"));
-            await message.AddReactionAsync(new Emoji("❌"));
-            PlayMessageID = message.Id;
-            
-        }
-
-        public async Task Join(IUser user) {
-            this.players.Add(user);
-            await LoggingService.LogAsync(new LogMessage(LogSeverity.Info, "Blackjack", $"{user.Username} has joined the game!"));
-            await ctx.Channel.SendMessageAsync($"{user.Mention} has joined the game!");
+            await playChannel.SendMessageAsync($"You are waiting in a lobby for players to join!");
+            lobbyMessage = await playChannel.SendMessageAsync($"Both players need to react with ✅ to start the game or ❌ to cancel.");
+            await lobbyMessage.AddReactionsAsync(new Emoji[] {new Emoji("✅"), new Emoji("❌")});
         }
 
         public async void AddPlayer(IUser user) {
+            if (playChannel == null) return;
             foreach (IUser userIn in players) {
                 if (userIn.Id == user.Id) {
                     return;
                 }
             }
             this.players.Add(user);
-            await channel.AddPermissionOverwriteAsync(user, new OverwritePermissions(viewChannel: PermValue.Allow));
-            await channel.SendMessageAsync($"{user.Mention} has joined the game!");
+            await playChannel.AddPermissionOverwriteAsync(user, new OverwritePermissions(viewChannel: PermValue.Allow));
+            await playChannel.SendMessageAsync($"{user.Mention} has joined the game!");
         }
     }
 }
