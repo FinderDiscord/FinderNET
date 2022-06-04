32bde4434056bbc8a60a79df566ce6ef7d2a9da0
Jake Gillman <57807696+JakeyGilly@users.noreply.github.com>
2022-06-04 08:55:09 +0000

    dates are wierd

--- FinderNET/Modules/Countdown.cs
+++ FinderNET/Modules/Countdown.cs
@@ -12,27 +12,49 @@ namespace FinderNET.Modules {
         public async Task CountdownCommand(string datetime) {
             var pharser = new Parser();
             var date = pharser.Parse(datetime);
-            if (date == null || date.Start == null) {
+            if (date == null || date.End == null) {
                 await RespondAsync("Invalid date or time");
                 return;
             }
-            if (date.Start.Value < DateTime.Now) {
+            if (date.End.Value < DateTime.Now) {
                 await RespondAsync("The date or time is in the past");
                 return;
             }
+            if (date.End.Value.Year - DateTime.Now.Year > 2) {
+                await RespondAsync("The date or time is too far in the future");
+                return;
+            }
+            string message = "";
+            await LoggingService.LogAsync(new LogMessage(LogSeverity.Info, "Countdown", $"{Context.User.Username} requested a countdown to {date.End.Value}"));
+            await LoggingService.LogAsync(new LogMessage(LogSeverity.Info, "Countdown", $"{int.Parse(date.End.Value.ToString("mm")) - int.Parse(DateTime.Now.ToString("mm"))} minute{(int.Parse(date.End.Value.ToString("mm")) == 1 ? "" : "s")}"));
+            if (int.Parse(date.End.Value.ToString("mm")) - int.Parse(DateTime.Now.ToString("mm")) > 0) {
+                message = $"{int.Parse(date.End.Value.ToString("mm")) - int.Parse(DateTime.Now.ToString("mm"))} minute{(int.Parse(date.End.Value.ToString("mm")) - int.Parse(DateTime.Now.ToString("mm")) == 1 ? "" : "s")} {message}";
+            }
+            await LoggingService.LogAsync(new LogMessage(LogSeverity.Info, "Countdown", $"{int.Parse(date.End.Value.ToString("hh")) - int.Parse(DateTime.Now.ToString("hh"))} hour{(int.Parse(date.End.Value.ToString("hh")) == 1 ? "" : "s")}"));
+            if (int.Parse(date.End.Value.ToString("hh")) - int.Parse(DateTime.Now.ToString("hh")) > 0) {
+                message = $"{int.Parse(date.End.Value.ToString("hh")) - int.Parse(DateTime.Now.ToString("hh"))} hour{(int.Parse(date.End.Value.ToString("hh")) == 1 ? "" : "s")} {message}";
+            }
+            await LoggingService.LogAsync(new LogMessage(LogSeverity.Info, "Countdown", $"{int.Parse(date.End.Value.ToString("dd")) - int.Parse(DateTime.Now.ToString("dd"))} day{(int.Parse(date.End.Value.ToString("dd")) == 1 ? "" : "s")}"));
+            if (int.Parse(date.End.Value.ToString("dd")) - int.Parse(DateTime.Now.ToString("dd")) > 0) {
+                message = $"{int.Parse(date.End.Value.ToString("dd")) - int.Parse(DateTime.Now.ToString("dd"))} day{(int.Parse(date.End.Value.ToString("dd")) == 1 ? "" : "s")} {message}";
+            }
+            await LoggingService.LogAsync(new LogMessage(LogSeverity.Info, "Countdown", $"{int.Parse(date.End.Value.ToString("MM")) - int.Parse(DateTime.Now.ToString("MM"))} month{(int.Parse(date.End.Value.ToString("MM")) == 1 ? "" : "s")}"));
+            if (int.Parse(date.End.Value.ToString("MM")) - int.Parse(DateTime.Now.ToString("MM")) > 0) {
+                message = $"{int.Parse(date.End.Value.ToString("MM")) - int.Parse(DateTime.Now.ToString("MM"))} month{(int.Parse(date.End.Value.ToString("MM")) == 1 ? "" : "s")} {message}";
+            }
+            await LoggingService.LogAsync(new LogMessage(LogSeverity.Info, "Countdown", $"{int.Parse(date.End.Value.ToString("yyyy")) - int.Parse(DateTime.Now.ToString("yyyy"))} year{(int.Parse(date.End.Value.ToString("yyyy")) == 1 ? "" : "s")}"));
+            if (int.Parse(date.End.Value.ToString("yyyy")) - int.Parse(DateTime.Now.ToString("yyyy")) > 0) {
+                message = $"{int.Parse(date.End.Value.ToString("yyyy")) - int.Parse(DateTime.Now.ToString("yyyy"))} year{(int.Parse(date.End.Value.ToString("yyyy")) == 1 ? "" : "s")} {message}";
+            }
+
+            await LoggingService.LogAsync(new LogMessage(LogSeverity.Info, "Countdown", $"{message}"));
             await RespondAsync("", embed: new EmbedBuilder() {
                 Title = "Countdown",
                 Color = Color.Orange,
                 Fields = new List<EmbedFieldBuilder> {
                     new EmbedFieldBuilder() {
-                        Name = "Date",
-                        Value = date.Start.Value.ToString("dddd, MMMM d, yyyy"),
-                        IsInline = true
-                    },
-                    new EmbedFieldBuilder() {
-                        Name = "Time",
-                        Value = date.Start.Value.ToString("h:mm tt"),
-                        IsInline = true
+                        Name = "Time left",
+                        Value = message
                     }
                 },
                 Footer = new EmbedFooterBuilder() {
