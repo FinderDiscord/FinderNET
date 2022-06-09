280606e435432815b14e48f62a7f203bfb818f5f
Jake Gillman <57807696+JakeyGilly@users.noreply.github.com>
2022-06-09 12:39:03 +0000

    added migrations

---
+++ FinderNET/Migrations/20220609123840_leveling.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FinderNET.Migrations
+{
+    /// <inheritdoc />
+    public partial class leveling : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "leveling",
+                columns: table => new
+                {
+                    guildId = table.Column<long>(type: "bigint", nullable: false),
+                    userId = table.Column<long>(type: "bigint", nullable: false),
+                    level = table.Column<int>(type: "integer", nullable: false),
+                    exp = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_leveling", x => new { x.guildId, x.userId });
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "leveling");
+        }
+    }
+}
