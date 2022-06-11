ebd4a59759f9e2d98d1836c15121fcb78222c021
Jake Gillman <57807696+JakeyGilly@users.noreply.github.com>
2022-06-11 21:42:14 +0000

    fix

--- FinderNET/Modules/Economy.cs
+++ FinderNET/Modules/Economy.cs
@@ -1,9 +1,9 @@
-using System;
 using Discord;
-using Discord.Commands;
+using Discord.Interactions;
+using Discord.WebSocket;
 using FinderNET.Database;
 
-namespace FinderNet.Modules {
+namespace FinderNET.Modules {
     public class EconomyModule : ModuleBase {
         public EconomyModule(DataAccessLayer dataAccessLayer) : base(dataAccessLayer) { }
     }
