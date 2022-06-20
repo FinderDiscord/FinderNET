429c301f545e9e309e17b4e65aba5a3d4ed25d67
Jake Gillman <57807696+JakeyGilly@users.noreply.github.com>
2022-06-20 10:19:13 +0100

    stuff
    stuff

--- FinderNET/Appsettings.cs
+++
@@ -1,9 +0,0 @@
-public class Appsettings {
-    public string token { get; set; } = "paste token here";
-    public ulong testGuild { get; set; } = 0;
-    public ConnectionStrings ConnectionStrings { get; set; } = new ConnectionStrings();
-}
-
-public class ConnectionStrings {
-    public string Default { get; set; } = "";
-}
